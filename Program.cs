8f27b2f8f855011697db07e1c397da4f1d1f035e
alim <7475149@mail.ru>
2022-09-08 19:16:00 +0600

    решена 1 задача

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите количeство чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+int count = 0;
+for (int i=0; i<m; i++)
+{
+    Console.WriteLine($"Введите число {i+1}");
+    array[i] = Convert.ToInt32(Console.ReadLine());    
+    if (array[i]>0) count +=1;
+   // Console.WriteLine($"число положительных {count}");
+}
+Console.Write("Массив [");
+for (int i=0; i<m;i++)
+{
+    Console.Write(array[i]+" ");
+
+}
+Console.Write("]");
+Console.WriteLine(" Количество положительных чисел - "+ count);
